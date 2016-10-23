02d8b7f2f421093e50f1469e4bf2c6368d30e8d9
Matt Bostock <matt@mattbostock.com>
2016-10-23 19:54:20 +0100

    Add remote write endpoint to ingest samples
    Add a 'remote write' endpoint that Prometheus can write to. For now,
    samples are not stored and are simply printed to stdout.
    
    The remove write server code is heavily based on the following example,
    which was duplicated verbatim in my previous commit:
    https://github.com/prometheus/prometheus/blob/f002fe186abd04ecd9cd8961c24bc039f134ea13/documentation/examples/remote_storage/server.go
    
    The acceptance test for the remote write endpoint is also heavily based
    on the code here:
    https://github.com/prometheus/prometheus/blob/f002fe186abd04ecd9cd8961c24bc039f134ea13/storage/remote/client.go#L64-L115

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
 	"golang.org/x/net/context"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/snappy"
 	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/common/route"
 	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage/local"
+	"github.com/prometheus/prometheus/storage/remote"
 	v1API "github.com/prometheus/prometheus/web/api/v1"
 )
 
-const apiRoute = "/api/v1"
+const (
+	apiRoute   = "/api/v1"
+	writeRoute = "/receive"
+)
 
 var config = struct {
 	listenAddr string
@@ -41,6 +50,32 @@ func main() {
 
 	var api = v1API.NewAPI(queryEngine, storage)
 	api.Register(router.WithPrefix(apiRoute))
+
+	router.Post("/receive", func(w http.ResponseWriter, r *http.Request) {
+		reqBuf, err := ioutil.ReadAll(snappy.NewReader(r.Body))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req remote.WriteRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		for _, ts := range req.Timeseries {
+			m := make(model.Metric, len(ts.Labels))
+			for _, l := range ts.Labels {
+				m[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+			}
+			fmt.Println(m)
+
+			for _, s := range ts.Samples {
+				fmt.Printf("  %f %d\n", s.Value, s.TimestampMs)
+			}
+		}
+	})
 
 	log.Fatal(http.ListenAndServe(config.listenAddr, router))
 }
