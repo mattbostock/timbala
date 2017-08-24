89ab1cc4ea673dd500afd32a3f8f02082354faee
Matt Bostock <matt@mattbostock.com>
2017-08-24 20:29:20 +0100

    Add ingestion routing
    Route data samples received to the peers responsible for them. As a
    first iteration, consider all peers to be responsible for every data
    sample.
    
    Use the data structure sampleNodeMap to group samples by node. I
    considered just sending data verbatim to peer (forward the payload
    as-is without any filtering) and let the receiving peer determine
    which series to accept, but decided not to as:
    
    a) the amount of additional network traffic could be significant
    
    b) it would make it difficult to know which series could not be ingested
    by the peers in the case of failure
    
    I'll need to come back to this later to tidy the code and optimise it.

--- internal/write/write.go
+++ internal/write/write.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"sort"
 	"sync"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/golang/protobuf/proto"
@@ -55,15 +56,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	appender, err := store.Appender()
-	if err != nil {
-		// FIXME: Make error more useful
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+	// FIXME handle change in cluster size
+	samplesToNodes := make(sampleNodeMap, len(cluster.Nodes()))
+	for _, n := range cluster.Nodes() {
+		samplesToNodes[*n] = make(seriesMap)
 	}
-	defer appender.Commit()
 
-	var samples uint32
 	for _, ts := range req.Timeseries {
 		m := make(labels.Labels, 0, len(ts.Labels))
 		for _, l := range ts.Labels {
@@ -75,33 +73,70 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		sort.Sort(m)
 
 		for _, s := range ts.Samples {
-			// FIXME: Look at using AddFast
-			appender.Add(m, s.TimestampMs, s.Value)
-			samples++
+			timestamp := time.Unix(s.TimestampMs/1000, (s.TimestampMs-s.TimestampMs/1000)*1e6)
+			// FIXME: Avoid panic if the cluster is not yet initialised
+			nodes := cluster.GetNodesForSeries(m, timestamp, timestamp)
+			for _, n := range nodes {
+				if _, ok := samplesToNodes[*n][m.Hash()]; !ok {
+					samplesToNodes[*n][m.Hash()] = &timeseries{labels: m}
+				}
+				samplesToNodes[*n][m.Hash()].samples = append(samplesToNodes[*n][m.Hash()].samples, s)
+			}
+		}
+		// FIXME: sort samples by time?
+	}
+
+	localSeries, ok := samplesToNodes[*cluster.LocalNode()]
+	if ok {
+		err = localWrite(localSeries)
+		if err != nil {
+			// FIXME make error more useful
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 	}
 
 	// This is an internal write, so don't replicate it to other nodes
 	if r.Header.Get(HttpHeaderInternalWrite) != "" {
-		log.Debugf("Received %d samples from another node in the cluster", samples)
+		// FIXME raise error if this is supposed to be an internal write but there were none?
+		log.Debugf("Received %d series from another node in the cluster", len(localSeries))
 		return
 	}
 
-	var wg sync.WaitGroup
-	// FIXME: Avoid panic if the cluster is not yet initialised
-	nodes := cluster.Nodes()
-	var wgErrChan = make(chan error, len(nodes))
-	for _, node := range nodes {
-		if node.Name() == cluster.LocalNode().Name() {
-			log.Debugf("Skipping local node %s", node)
-			continue
-		}
+	err = remoteWrite(samplesToNodes)
+	if err != nil {
+		// FIXME make error more useful
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
 
+func localWrite(series seriesMap) error {
+	appender, err := store.Appender()
+	if err != nil {
+		return err
+	}
+
+	for _, sseries := range series {
+		for _, s := range sseries.samples {
+			// FIXME: Look at using AddFast
+			appender.Add(sseries.labels, s.TimestampMs, s.Value)
+		}
+	}
+	// Intentionally avoid defer on hot path
+	appender.Commit()
+	return nil
+}
+
+func remoteWrite(sampleMap sampleNodeMap) error {
+	var wg sync.WaitGroup
+	var wgErrChan = make(chan error, len(cluster.Nodes()))
+	for node, nodeSamples := range sampleMap {
 		wg.Add(1)
-		go func(n *cluster.Node) {
+		go func(n cluster.Node, nSamples seriesMap) {
 			defer wg.Done()
 
-			log.Debugf("Writing %d samples to %s", samples, n)
+			log.Debugf("Writing %d samples to %s", len(nSamples), n.Name())
 
 			httpAddr, err := n.HTTPAddr()
 			if err != nil {
@@ -109,6 +144,39 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			apiURL := fmt.Sprintf("%s%s%s", "http://", httpAddr, Route)
+
+			req := &remote.WriteRequest{
+				Timeseries: make([]*remote.TimeSeries, 0, len(nSamples)),
+			}
+			for _, series := range nSamples {
+				for _, s := range series.samples {
+					ts := &remote.TimeSeries{
+						Labels: make([]*remote.LabelPair, 0, len(series.labels)),
+					}
+					for _, l := range series.labels {
+						ts.Labels = append(ts.Labels,
+							&remote.LabelPair{
+								Name:  l.Name,
+								Value: l.Value,
+							})
+					}
+					ts.Samples = []*remote.Sample{
+						{
+							Value:       float64(s.Value),
+							TimestampMs: int64(s.TimestampMs),
+						},
+					}
+					req.Timeseries = append(req.Timeseries, ts)
+				}
+			}
+
+			data, err := proto.Marshal(req)
+			if err != nil {
+				wgErrChan <- err
+				return
+			}
+
+			compressed := snappy.Encode(nil, data)
 			nodeReq, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(compressed))
 			if err != nil {
 				wgErrChan <- err
@@ -116,6 +184,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			}
 			nodeReq.Header.Add("Content-Encoding", "snappy")
 			nodeReq.Header.Set("Content-Type", "application/x-protobuf")
+			// FIXME move version numbers into constants
 			nodeReq.Header.Set("X-Prometheus-Remote-Write-Version", "0.1.0")
 			nodeReq.Header.Set(HttpHeaderInternalWrite, "0.0.1")
 
@@ -126,15 +195,25 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			defer httpResp.Body.Close()
-		}(node)
+		}(node, nodeSamples)
 	}
 	// FIXME cancel requests if one fails
 	wg.Wait()
 
 	select {
 	case err := <-wgErrChan:
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return err
 	default:
 	}
+
+	return nil
+}
+
+type sampleNodeMap map[cluster.Node]seriesMap
+
+type seriesMap map[uint64]*timeseries
+
+type timeseries struct {
+	labels  labels.Labels
+	samples []*remote.Sample
 }
