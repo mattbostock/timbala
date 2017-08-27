8851e42d8b3a787182e5894b68e038509775fc01
Matt Bostock <matt@mattbostock.com>
2017-08-27 21:36:25 +0100

    Cluster: Add hash ring
    Add a hash ring using the `consistenthash` package that
    deterministically partitions and replicates data samples across nodes in
    the cluster.
    
    Includes a test for a good distribution of data across all nodes in the
    cluster, using 10,000 data samples spaced at 24-hour intervals. The
    number 10,000 was chosen as a reasonable number of samples that would
    not take too much time (or memory) to generate and test with.
    
    The 24-hour interval is useful in testing the consistent hashing
    algorithm and number of virtual nodes (determined by `hashringVnodes`),
    rather than the partition key which hashes based on 24-hour intervals. I
    should expand these tests later to better test the partition key.
    
    A replication factor of 3 (specified by the constant
    `replicationFactor`) means that all data in the cluster will be stored
    in 3 copies, which in ideal conditions will be spread across the cluster
    on at least 3 separate nodes, though that is not guaranteed by the
    consistent hashing algorithm.
    
    The replication factor is a trade-off between the financial cost of
    storing additional copies versus the likelihood of losing data. The
    choice of 3 replicas is based on industry norms. I have no plans to make
    this constant configurable by the user in a bid to keep the
    configuration simple.
    
    Nodes leaving the cluster is not yet handled, I'll tackle that
    separately.
    
    See #27 for more details about the consistent hashing algorithm chosen
    and see #12 for more details about the choice of partition key.
    
    Closes #57.

---
+++ internal/cluster/cluster_test.go
@@ -0,0 +1,97 @@
+package cluster
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/golang/groupcache/consistenthash"
+	"github.com/mattbostock/athensdb/internal/test/testutil"
+	"github.com/montanaflynn/stats"
+	"github.com/prometheus/common/model"
+)
+
+const (
+	numSamples   = 1e5
+	numTestNodes = 11
+)
+
+func TestHashringDistribution(t *testing.T) {
+	samples := testutil.GenerateDataSamples(numSamples, 1, 24*time.Hour)
+	testSampleDistribution(t, 1, samples)
+	testSampleDistribution(t, replicationFactor, samples)
+}
+
+func testSampleDistribution(t *testing.T, replFactor int, samples []model.Sample) {
+	var buckets [][]model.Sample
+	c.ring = consistenthash.New(replicationFactor*hashringVnodes, nil)
+	// Add mock nodes to ring
+	for i := 0; i < numTestNodes; i++ {
+		c.ring.Add(strconv.Itoa(i))
+		// Make buckets large enough to hold a node's share plus some extra if distribution is poor
+		buckets = append(buckets, make([]model.Sample, 0, (numSamples/(numTestNodes-1))))
+	}
+
+	for _, s := range samples {
+		for i := 0; i < replFactor; i++ {
+			// FIXME: Work out chunk end timestamp
+			node, err := strconv.Atoi(c.ring.Get(strconv.Itoa(i) + SeriesPrimaryKey([]byte(""), s.Timestamp.Time())))
+			if err != nil {
+				t.Fatal(err)
+			}
+			buckets[node] = append(buckets[node], s)
+		}
+	}
+
+	fmt.Printf("Distribution of samples when replication factor is %d:\n\n", replFactor)
+	var sampleData stats.Float64Data
+
+	for i := 0; i < len(buckets); i++ {
+		percent := float64(len(buckets[i])) / float64(len(samples)*replFactor) * 100
+		fmt.Printf("Node %-2d: %-100s %5.2f%%; %d samples\n", i, strings.Repeat("#", int(percent)), percent, len(buckets[i]))
+		sampleData = append(sampleData, float64(len(buckets[i])))
+	}
+
+	min, err := sampleData.Min()
+	if err != nil {
+		t.Fatal(err)
+	}
+	max, err := sampleData.Max()
+	if err != nil {
+		t.Fatal(err)
+	}
+	mean, err := sampleData.Mean()
+	if err != nil {
+		t.Fatal(err)
+	}
+	median, err := sampleData.Median()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stddev, err := sampleData.StandardDeviationPopulation()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sum, err := sampleData.Sum()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("\nMin: %.0f", min)
+	fmt.Printf("\nMax: %.0f", max)
+	fmt.Printf("\nMean: %.2f", mean)
+	fmt.Printf("\nMedian: %.0f", median)
+	fmt.Printf("\nStandard deviation: %.2f", stddev)
+	fmt.Printf("\nTotal samples: %.0f\n\n", sum)
+
+	if min == 0 {
+		t.Fatal("Some nodes received zero samples")
+	}
+	if sum != float64(numSamples*replFactor) {
+		t.Fatalf("Not all samples accounted for, found %.0f but expected %.0f", sum, numSamples*replFactor)
+	}
+	if stddev > float64(numSamples/10) {
+		t.Fatalf("Samples not well distributed, standard deviation is %.2f for %d samples over %d nodes", stddev, numSamples*replFactor, numTestNodes)
+	}
+}
