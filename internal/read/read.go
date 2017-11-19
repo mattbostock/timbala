8d5d58e87002b37fa45129450674cf41e72b8966
Matt Bostock <matt@mattbostock.com>
2017-11-19 20:10:29 +0000

    Update Prometheus dependencies
    Prometheus 2.0.0 was released; update dependencies accordingly and use
    the same version of the `tsdb` library as Prometheus 2.0.0.
    
    Removes some functions from `internal/read/read.go` that are now
    exported in an upstream package.

--- internal/read/read.go
+++ internal/read/read.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/golang/snappy"
 	"github.com/mattbostock/timbala/internal/cluster"
-	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/storage/remote"
 	"github.com/sirupsen/logrus"
 )
 
@@ -97,7 +97,7 @@ func (re *reader) HandlerFunc(w http.ResponseWriter, r *http.Request) {
 		defer querier.Close()
 
 		sset := querier.Select(matchers...)
-		resp.Results[i], err = ToQueryResult(sset)
+		resp.Results[i], err = remote.ToQueryResult(sset)
 		if err != nil {
 			re.log.Warning(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -125,52 +125,8 @@ func (re *reader) HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// BEGIN FIXME: Use upstream versions of the following functions once they hit the dev-2.0 branch
-// See: https://github.com/prometheus/prometheus/commit/639d5c6f98a6bf9790dbcc8d29d2ebcc14e40995
-
-// FromQuery unpacks a Query proto.
-func FromQuery(req *prompb.Query) (model.Time, model.Time, []*labels.Matcher, error) {
-	matchers, err := fromLabelMatchers(req.Matchers)
-	if err != nil {
-		return 0, 0, nil, err
-	}
-	from := model.Time(req.StartTimestampMs)
-	to := model.Time(req.EndTimestampMs)
-	return from, to, matchers, nil
-}
-
-// ToQueryResult builds a QueryResult proto.
-func ToQueryResult(sset storage.SeriesSet) (*prompb.QueryResult, error) {
-	resp := &prompb.QueryResult{}
-
-	for sset.Next() {
-		si := sset.At().Iterator()
-		if err := si.Err(); err != nil {
-			return nil, err
-		}
-
-		ts := prompb.TimeSeries{
-			Labels: ToLabelPairs(sset.At().Labels()),
-		}
-		for si.Next() {
-			t, v := si.At()
-			ts.Samples = append(ts.Samples, &prompb.Sample{
-				Timestamp: t,
-				Value:     v,
-			})
-		}
-		if err := si.Err(); err != nil {
-			return nil, err
-		}
-
-		resp.Timeseries = append(resp.Timeseries, &ts)
-	}
-	if err := sset.Err(); err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
+// BEGIN FIXME: Use upstream versions of the following functions once they are exported
+// See: github.com/prometheus/prometheus/storage/remote
 func fromLabelMatchers(matchers []*prompb.LabelMatcher) ([]*labels.Matcher, error) {
 	result := make([]*labels.Matcher, 0, len(matchers))
 	for _, matcher := range matchers {
@@ -219,27 +175,6 @@ func toLabelMatchers(matchers []*labels.Matcher) ([]*prompb.LabelMatcher, error)
 		})
 	}
 	return result, nil
-}
-
-// ToLabelPairs builds a []LabelPair from a model.Metric
-func ToLabelPairs(lbls labels.Labels) []*prompb.Label {
-	labels := make([]*prompb.Label, 0, len(lbls))
-	for _, l := range lbls {
-		labels = append(labels, &prompb.Label{
-			Name:  l.Name,
-			Value: l.Value,
-		})
-	}
-	return labels
-}
-
-// FromLabelPairs unpack a []LabelPair to a model.Metric
-func FromLabelPairs(lbls []*prompb.Label) model.Metric {
-	metric := make(model.Metric, len(lbls))
-	for _, l := range lbls {
-		metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
-	}
-	return metric
 }
 
 // £ND FIXME
